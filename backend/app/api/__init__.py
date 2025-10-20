a9cf5f20dae51a16ead8be23fc91c413cc09409c
HubbleBana <eddisala@hotmail.it>
2025-10-20 13:14:54 +0200

    chore(api): include /models/ollama router in API top-level

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,1 +1,9 @@
-﻿
+from fastapi import APIRouter
+
+from app.api import routes as routes_main
+from app.api import models_ollama
+
+router = APIRouter()
+
+router.include_router(routes_main.router, prefix="")
+router.include_router(models_ollama.router, prefix="")
